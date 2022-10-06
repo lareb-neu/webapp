fe40dd66acdbbbc4367a7e3c7961453a4194a711
larebkhan1996 <khan.la@northeastern.edu>
2022-10-06 15:18:44 -0400

    edit code for health

--- test_health.py
+++ test_health.py
@@ -2,4 +2,4 @@ from app import app # Flask instance of the API
 
 def test_index_route():
     response = app.test_client().get('/healthz')
-    assert response.status_code == 200
+    assert response.status_code == 201
